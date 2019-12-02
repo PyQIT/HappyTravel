77971df3a9c6cbfd5e9f07cc1d03ac4b9a86b5d0
Przemysław Pyk <pyk.przemyslaw@gmail.com>
2019-12-02 23:15:55 +0100

    [06-feature] - dto and transformer

---
+++ Backend/src/main/java/com/happytravel/happytravel/api/dto/ReservationDto.java
@@ -0,0 +1,28 @@
+package com.happytravel.happytravel.api.dto;
+
+import com.happytravel.happytravel.api.model.enums.*;
+import lombok.Data;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@Data
+public class ReservationDto {
+
+    private Long id;
+    private Long clientID;
+    private Long sellerID;
+    private Long travelID;
+    private Long adultNumber;
+    private Long childrenNumber;
+    @Enumerated(EnumType.STRING)
+    private CateringType cateringType;
+    @Enumerated(EnumType.STRING)
+    private AlcoholType alcoholType;
+    @Enumerated(EnumType.STRING)
+    private EntertaimentType entertaimentType;
+    @Enumerated(EnumType.STRING)
+    private RatingType ratingType;
+    @Enumerated(EnumType.STRING)
+    private PaymentType paymentType;
+}
