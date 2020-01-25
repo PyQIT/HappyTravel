15cad2a8e03a016746bef4760312353ab717dd3c
arturfilipd <arturfilipd@gmail.com>
2020-01-25 16:00:17 +0100

    merge z /testy2/

--- Frontend/src/Navigation/NavMenu.js
+++ Frontend/src/Navigation/NavMenu.js
@@ -4,6 +4,7 @@ import Employees from "../Pages/Manager/Employees/Employees";
 import Offices from "../Pages/Manager/Office/Offices";
 import UserSettings from "../Pages/User/UserSettings";
 import TripList from "../Pages/Manager/Trips/TripList";
+import AvailableTravels from "../Pages/Customer/AviableTravels";
 
 class NavMenu extends React.Component{
 
@@ -21,7 +22,7 @@ class NavMenu extends React.Component{
         if(this.props.type === 'Klient'){
             return(
                 <div>
-                    <button className='panelButton'>Browse</button>
+                    <button className='panelButton' onMouseDown = {() => this.props.switch(<AvailableTravels loggedUser={this.props.loggedUser} switch={this.props.switch}/>)}>Browse</button>
                     <button className='panelButton'>My Trips</button>
                     <button className='panelButton' onMouseDown={()=> this.props.switch(<UserSettings loggedUser={this.props.loggedUser} switch={this.props.switch}/>)}>Account</button>
                     <button className='panelButton'>About us</button>
@@ -35,7 +36,7 @@ class NavMenu extends React.Component{
                     <button className='panelSpecialButton' onMouseDown={()=> this.props.switch(<Employees loggedUser={this.props.loggedUser.id} switch={this.props.switch}/>)}>Employees</button>
                     <button className='panelSpecialButton' onMouseDown={()=> this.props.switch(<Offices loggedUser={this.props.loggedUser.id} switch={this.props.switch}/>)}>Offices</button>
                     <button className='panelSpecialButton' onMouseDown={()=> this.props.switch(<TripList loggedUser={this.props.loggedUser.id} switch={this.props.switch}/>)}>Trips</button><hr/>
-                    <button className='panelButton'>Browse</button>
+                    <button className='panelButton' onMouseDown = {() => this.props.switch(<AvailableTravels loggedUser={this.props.loggedUser} switch={this.props.switch}/>)}>Browse</button>
                     <button className='panelButton' onMouseDown={()=> this.props.switch(<UserSettings loggedUser={this.props.loggedUser} switch={this.props.switch}/>)}>Account</button>
                     <button className='panelButton'>About us</button>
                     <button className='panelButton'>Contact</button>
@@ -46,7 +47,7 @@ class NavMenu extends React.Component{
             return(
                 <div>
                     <button className='panelSpecialButton'>Reserve</button>
-                    <button className='panelButton'>Browse</button>
+                    <button className='panelButton' onMouseDown = {() => this.props.switch(<AvailableTravels loggedUser={this.props.loggedUser} switch={this.props.switch}/>)}>Browse</button>
                     <button className='panelButton' onMouseDown={()=> this.props.switch(<UserSettings loggedUser={this.props.loggedUser} switch={this.props.switch}/>)}>Account</button>
                     <button className='panelButton'>About us</button>
                     <button className='panelButton'>Contact</button>
